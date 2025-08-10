af9a9fd615c888b9d8e738ad8863cfcffe8392d1
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-10 07:31:38 +0600

    php project

--- step-1/pages/admin/users/manage_users.php
+++ step-1/pages/admin/users/manage_users.php
@@ -1,23 +1,3 @@
-<!-- header  -->
-<?php
-include("include/header.php")
-?>
-<!-- /.header -->
-
-
-<!-- Navbar  -->
-<?php
-include("include/nav.php")
-?>
-<!-- /.navbar -->
-
-<!-- Main Sidebar Container -->
-<?php
-include("include/sidebar.php")
-?>
-
-<!-- ./Main Sidebar Container -->
-
 
 <!-- Add Users -->
 
@@ -68,15 +48,3 @@ include("include/sidebar.php")
     <!-- /.content -->
   </div>
   <!-- ./Add users -->
-<!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-</aside>
-<!-- /.control-sidebar -->
-
-<!-- Main Footer -->
-<?php
-include("include/footer.php");
-?>
-
-<!-- Main Footer -->
