9cccd4efbe93252140737d82ff5a9dae6ab57ba6
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-26 12:40:40 +0600

    project

--- step-1/index.php
+++ step-1/index.php
@@ -1,35 +1,10 @@
 <?php 
 require_once("include/portal/header.php");
 ?>
-    <div class="top-bar d-none d-md-block">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="top-bar-left">
-                        <div class="text">
-                            <i class="fa fa-phone-alt"></i>
-                            <p>+880 12543 41534</p>
-                        </div>
-                        <div class="text">
-                            <i class="fa fa-envelope"></i>
-                            <p>donorhub@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="top-bar-right">
-                        <div class="social">
-                            <a href="#" data-bs-toggle="tooltip" title="Twitter"><i class="bi bi-twitter-x"></i></i></a>
-                            <a href="#" data-bs-toggle="tooltip" title="Facebook"><i class="bi bi-facebook"></i></i></a>
-                            <a href="#" data-bs-toggle="tooltip" title="LinkedIn"><i class="bi bi-linkedin"></i></i></a>
-                            <a href="#" data-bs-toggle="tooltip" title="Instagram"><i class="bi bi-instagram"></i></i></a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
+   
+<?php 
+require_once("include/portal/topbar.php");
+?>
     <div class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
             
