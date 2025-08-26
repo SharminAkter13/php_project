353db1352adb20fe6268bc413bdee387f4eec560
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-26 13:01:10 +0600

    project

--- step-1/index.php
+++ step-1/index.php
@@ -20,121 +20,15 @@ require_once("pages/portal/service.php");
 <?php 
 require_once("pages/portal/facts.php");
 ?>
+<?php 
+require_once("pages/portal/causes.php");
+?>
    
     
    
    
     
-   <!-- Causes Start -->
-<div class="container-fluid causes">
-    <div class="container py-5">
-        <div class="text-center mx-auto pb-5" style="max-width: 800px;">
-            <h5 class="text-uppercase text-custom">Recent Causes</h5>
-            <h1 class="mb-4">The environment needs our protection</h1>
-            <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-            </p>
-        </div>
-        <div class="row g-2">
-            <div class="col-lg-3 col-md-6">
-                <div class="causes-item">
-                    <div class="causes-img">
-                        <img src="assets/img/img/cause-1.jpg" class="img-fluid w-100" alt="Image">
-                        <div class="causes-link pb-2 px-3">
-                            <small class="text-dark"><i class="fas fa-chart-bar text-custom me-2"></i>Goal: $3600</small>
-                            <small class="text-dark"><i class="fa fa-thumbs-up text-custom me-2"></i>Raised: $4000</small>
-                        </div>
-                        <div class="causes-dination p-2">
-                            <a class="btn-hover-bg btn btn-custom text-white py-2 px-3" href="#">Donate Now</a>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                            <span>65%</span>
-                        </div>
-                    </div>
-                    <div class="causes-content p-4">
-                        <h4 class="mb-3">First environments activity of</h4>
-                        <p class="mb-4">Help today because tomorrow you may be the one who needs more helping! </p><br>
-                        <a class="btn-hover-bg btn btn-custom text-white py-2 px-3" href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="causes-item">
-                    <div class="causes-img">
-                        <img src="assets/img/img/cause-2.png" class="img-fluid w-100" alt="Image">
-                        <div class="causes-link pb-2 px-3">
-                            <small class="text-dark"><i class="fas fa-chart-bar text-custom me-2"></i>Goal: $3600</small>
-                            <small class="text-dark"><i class="fa fa-thumbs-up text-custom me-2"></i>Raised: $4000</small>
-                        </div>
-                        <div class="causes-dination p-2">
-                            <a class="btn-hover-bg btn btn-custom text-white py-2 px-3" href="#">Donate Now</a>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <span>75%</span>
-                        </div>
-                    </div>
-                    <div class="causes-content p-4">
-                        <h4 class="mb-3">Build school for poor children.</h4>
-                        <p class="mb-4">Help today because tomorrow you may be the one who needs more helping!</p><br>
-                        <a class="btn-hover-bg btn btn-custom text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="causes-item">
-                    <div class="causes-img">
-                        <img src="assets/img/img/cause-3.jpg" class="img-fluid w-100" alt="Image">
-                        <div class="causes-link pb-2 px-3">
-                            <small class="text-dark"><i class="fas fa-chart-bar text-custom me-2"></i>Goal: $3600</small>
-                            <small class="text-dark"><i class="fa fa-thumbs-up text-custom me-2"></i>Raised: $4000</small>
-                        </div>
-                        <div class="causes-dination p-2">
-                            <a class="btn-hover-bg btn btn-custom text-white py-2 px-3" href="#">Donate Now</a>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-                            <span>85%</span>
-                        </div>
-                    </div>
-                    <div class="causes-content p-4">
-                        <h4 class="mb-3">Building clean-water system for rural poor.</h4>
-                        <p class="mb-4">Help today because tomorrow you may be the one who needs more helping!</p>
-                        <a class="btn-hover-bg btn btn-custom text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="causes-item">
-                    <div class="causes-img">
-                        <img src="assets/img/img/cause-4.jpg" class="img-fluid w-100" alt="Image">
-                        <div class="causes-link pb-2 px-3">
-                            <small class="text-dark"><i class="fas fa-chart-bar text-custom me-2"></i>Goal: $3600</small>
-                            <small class="text-dark"><i class="fa fa-thumbs-up text-custom me-2"></i>Raised: $4000</small>
-                        </div>
-                        <div class="causes-dination p-2">
-                            <a class="btn-hover-bg btn btn-custom text-white py-2 px-3" href="#">Donate Now</a>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
-                            <span>95%</span>
-                        </div>
-                    </div>
-                    <div class="causes-content p-4">
-                        <h4 class="mb-3">First environments activity of this summer.</h4>
-                        <p class="mb-4">Help today because tomorrow you may be the one who needs more helping!</p>
-                        <a class="btn-hover-bg btn btn-custom text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>        <!-- Causes End -->
-
+  
     <div class="donate" data-parallax="scroll" data-image-src="assets/img/donate.jpg">
         <div class="container">
             <div class="row align-items-center">
