ef33472d0ef5c0538e76438c8a3b1334d148188c
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-26 13:06:38 +0600

    project

--- step-1/index.php
+++ step-1/index.php
@@ -26,60 +26,15 @@ require_once("pages/portal/causes.php");
 <?php 
 require_once("pages/portal/donate.php");
 ?>
+<?php 
+require_once("pages/portal/event.php");
+?>
    
     
    
    
     
   
-    <div class="event">
-        <div class="container">
-            <div class="section-header text-center">
-                <p>Upcoming Events</p>
-                <h2>Be ready for our upcoming charity events</h2>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="event-item">
-                        <img src="assets/img/img/event.jpg" alt="Image">
-                        <div class="event-content">
-                            <div class="event-meta">
-                                <p><i class="fa fa-calendar-alt"></i>17-Aug-25</p>
-                                <p><i class="far fa-clock"></i>8:00 - 10:00</p>
-                                <p><i class="fa fa-map-marker-alt"></i>New York</p>
-                            </div>
-                            <div class="event-text">
-                                <h3>Uniting Voices for Global Democracy: Pledge4Peace Conference 2025</h3>
-                                <p>
-                                    Be part of a global gathering dedicated to resolving conflict, defending democracy, and building lasting peace through people power
-                                </p>
-                                <a class="btn btn-custom" href="">Join Now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="event-item">
-                        <img src="assets/img/img/event-2.jpg" alt="Image">
-                        <div class="event-content">
-                            <div class="event-meta">
-                                <p><i class="fa fa-calendar-alt"></i>20-Aug-25</p>
-                                <p><i class="far fa-clock"></i>8:00 - 10:00</p>
-                                <p><i class="fa fa-map-marker-alt"></i>New York</p>
-                            </div>
-                            <div class="event-text">
-                                <h3>Humanitarian Aid in Gaza From an Aid Worker Perspective</h3>
-                                <p>
-                                    Please join us for a timely and important Zoom event featuring Ella Watson-Stryker of Médecins Sans Frontières (Doctors Without Borders) who 
-                                <br><a class="btn btn-custom" href="">Join Now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!-- Team Section -->
     
     <div class="team">
