4acb20e3eb0e108946078e675dcef282e3c89467
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-26 12:39:03 +0600

    project

---
+++ step-1/include/portal/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>DonorHub_Donation Website</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <link href="assets/img/img/main-logo.png" rel="icon">
+
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="assets/lib/flaticon/font/flaticon.css" rel="stylesheet">
+    <link href="assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+ 
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
+</head>
+
+<body>
---
+++ step-1/include/portal/nav.php
@@ -0,0 +1,58 @@
+<!-- Messages Dropdown Menu -->
+<li class="nav-item dropdown">
+  <a class="nav-link" data-toggle="dropdown" href="#">
+    <i class="far fa-comments"></i>
+    <span class="badge badge-danger navbar-badge">3</span>
+  </a>
+  <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+    <!-- Messages content goes here -->
+    <!-- Same as before... -->
+  </div>
+</li>
+
+<!-- Notifications Dropdown Menu -->
+<li class="nav-item dropdown">
+  <a class="nav-link" data-toggle="dropdown" href="#">
+    <i class="far fa-bell"></i>
+    <span class="badge badge-warning navbar-badge">15</span>
+  </a>
+  <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+    <!-- Notifications content goes here -->
+    <!-- Same as before... -->
+  </div>
+</li>
+
+<!-- Fullscreen Button -->
+<li class="nav-item">
+  <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+    <i class="fas fa-expand-arrows-alt"></i>
+  </a>
+</li>
+
+<!-- Sidebar Toggle -->
+<li class="nav-item">
+  <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+    <i class="fas fa-th-large"></i>
+  </a>
+</li>
+<!-- User Account Dropdown -->
+<li class="nav-item dropdown">
+  <a class="nav-link" data-toggle="dropdown" href="#">
+    <i class="fas fa-user-circle fa-lg"></i>
+  </a>
+  <div class="dropdown-menu dropdown-menu-right">
+    <a class="dropdown-item" href="home.php?page=28">
+      <i class="fas fa-user mr-2"></i> View Profile
+    </a>
+    <a class="dropdown-item" href="manage_system.php">
+      <i class="fas fa-cogs mr-2"></i> Manage System
+    </a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item text-danger" href="home.php?page=27">
+      <i class="fas fa-sign-out-alt mr-2"></i> Logout
+    </a>
+  </div>
+</li>
+
+</ul>
+</nav>
