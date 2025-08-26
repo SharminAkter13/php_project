78fe9d02c28cb3e910a1584d3773e7a5fcd6563c
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-26 12:49:12 +0600

    project

---
+++ step-1/pages/portal/top_vg.php
@@ -0,0 +1,15 @@
+<section class="top-bg">
+      <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="assets/img/img/bgvd.mp4" type="video/mp4" /></video>
+        <div class="masthead">
+            <div class="masthead-content text-white">
+                <div class="container-fluid px-4 px-lg-0">
+                    <h1 class="fst-italic lh-1 mb-4 text-white">
+                        Give a Little, Change a Life</h1>
+                     <h3 class="fst-italic lh-1 mb-4 text-white">  Together for Better Tomorrows</h3>
+                    
+                    <p class="mb-5">We're working hard to help the people .So stay with us</p>
+                    <a href=""><button style="width: 130px; height:50px;">Donote Now</button></a>
+                </div>
+            </div>
+        </div>
+    </section>
