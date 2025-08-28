82f9a7e9843edff6c492473c39c60ab4d774b584
SharminAkter13 <sharminakter13092000@gmail.com>
2025-08-28 10:15:57 +0600

    project proxy

--- step-1/pages/portal/volunteer.php
+++ step-1/pages/portal/volunteer.php
@@ -1,37 +1,48 @@
-  <div class="volunteer" data-parallax="scroll" data-image-src="assets/img/img/volunteer.jpg">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-5">
-                    <div class="volunteer-form">
-                        <form>
-                            <div class="control-group">
-                                <input type="text" class="form-control" placeholder="Name" required="required" />
-                            </div>
-                            <div class="control-group">
-                                <input type="email" class="form-control" placeholder="Email" required="required" />
-                            </div>
-                            <div class="control-group">
-                                <textarea class="form-control" placeholder="Why you want to become a volunteer?" required="required"></textarea>
-                            </div>
-                            <div>
-                                <button class="btn btn-custom" type="submit">Become a volunteer</button>
-                            </div>
-                        </form>
-                    </div>
+<?php
+$is_logged_in = isset($_SESSION['user_id']); 
+?>
+
+<div class="volunteer" data-parallax="scroll" data-image-src="assets/img/img/volunteer.jpg">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-5">
+                <div class="volunteer-form rounded">
+                    <form action="" method="POST">
+                        <div class="control-group btn-outline-dark">
+                            <input type="text" class="form-control" placeholder="Name" name="name" required />
+                        </div>
+                        <div class="control-group btn-outline-dark">
+                            <input type="email" class="form-control" placeholder="Email" name="email" required />
+                        </div>
+                        <div class="control-group btn-outline-dark">
+                            <textarea class="form-control" placeholder="Why do you want to become a volunteer?" name="reason" rows="4" required></textarea>
+                        </div>
+
+                        <div>
+                            <?php if ($is_logged_in): ?>
+                                <!-- Show actual submit if logged in -->
+                                <button class="btn btn-custom btn-outline-light" type="submit" formaction="volunteer_submit.php">Become a Volunteer</button>
+                            <?php else: ?>
+                                <!-- Prompt login if not logged in -->
+                                <button class="btn btn-custom btn-outline-light" type="submit" formaction="login.php">Log in/Register to Volunteer</button>
+                            <?php endif; ?>
+                        </div>
+                    </form>
                 </div>
-                <div class="col-lg-7">
-                    <div class="volunteer-content">
-                        <div class="section-header">
-                            <p>Become A Volunteer</p>
-                            <h2>Let’s make a difference in the lives of others</h2>
-                        </div>
-                        <div class="volunteer-text">
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non. Aliquam metus tortor, auctor id gravida, viverra quis sem. Curabitur non nisl nec nisi maximus. Aenean convallis porttitor. Aliquam interdum at lacus non blandit.
-                            </p>
-                        </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="volunteer-content">
+                    <div class="section-header">
+                        <p>Become A Volunteer</p>
+                        <h2>Let’s make a difference in the lives of others</h2>
+                    </div>
+                    <div class="volunteer-text">
+                        <p>
+                            Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non. Aliquam metus tortor, auctor id gravida, viverra quis sem. Curabitur non nisl nec nisi maximus. Aenean convallis porttitor. Aliquam interdum at lacus non blandit.
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
